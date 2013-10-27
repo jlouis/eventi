02e9f6e00ccfdd53f4989b3d3e5111337105fd22
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2013-10-27 19:19:37 +0100

    Implement a silly read/write scheme.

--- src/eventi_srv.erl
+++ src/eventi_srv.erl
@@ -6,6 +6,8 @@
 	code_change/3, handle_call/3, handle_cast/2, handle_info/2, init/1, terminate/2
 	]).
 	
+-define(TIMEOUT, 128 * 1000).
+
 -export([
 	start_link/0,
 	handle_msg/1
@@ -32,6 +34,22 @@ start_link() ->
 handle_msg({t_hello, Tag, eventi_02, _Uid, _Strength, _Crypto, _Codec}) ->
 	{reply, {r_hello, Tag, <<"eventi">>, 0,0}};
 handle_msg({t_ping, Tag}) -> {reply, {r_ping, Tag}};
+handle_msg({t_write, Tag, _Type, _Pad, Data}) ->
+	Score = score(Data),
+	ok = gen_server:call(?MODULE, {write, Score, Data}, ?TIMEOUT),
+	{reply, {r_write, Tag, Score}};
+handle_msg({t_read, Tag, Score, _Type, _Pad, Count}) ->
+	case gen_server:call(?MODULE, {read, Score}, ?TIMEOUT) of
+		{ok, Data} when byte_size(Data) =< Count ->
+		  {reply, {r_read, Tag, Data}};
+		{ok, _Data} ->
+		  {reply, {r_error, Tag, <<"Count Exceeded">>}};
+		not_found ->
+		  {reply, {r_error, Tag, <<"Data not found">>}}
+	end;
+handle_msg({t_sync, Tag}) ->
+	%% Current setup makes every call sync for simplicity
+	{reply, {r_sync, Tag}};
 handle_msg({t_goodbye, _Tag}) -> {stop, goodbye}.
 
 %% Callbacks
@@ -40,6 +58,12 @@ init([VentiDir]) ->
 	{ok, DBRef} = eleveldb:open(VentiDir, [{create_if_missing, true}]),
 	{ok, #state{ db = DBRef }}.
 	
+handle_call({write, Score, Data}, _From, #state { db = Db } = State) ->
+	ok = eleveldb:write(Db, [{put, Score, Data}], [{sync, true}]),
+	{reply, ok, State};
+handle_call({read, Score}, _From, #state { db = Db } = State) ->
+	R = eleveldb:read(Db, Score, [{verify_checksums, true}, {fill_cache, true}]),
+	{reply, R, State};
 handle_call(Msg, _From, State) ->
 	_ = lager:error("Unknown call: ~p", [Msg]),
 	{reply, {error, unknown}, State}.
@@ -57,4 +81,9 @@ terminate(_Reason, _State) ->
 	
 code_change(_OldVsn, State, _Extra) ->
 	{ok, State}.
+	
+%% Internal
+
+score(B) when is_binary(B) ->
+	crypto:hash(sha1, B).
 	
